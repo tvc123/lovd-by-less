960a0199e3ca79311d7278fd5a4996ebf14a9017
jbasdf <jbasdf@cosls-macbook-pro.local>
2008-06-17 14:10:11 -0600

    updated profiles

--- app/helpers/friends_helper.rb
+++ app/helpers/friends_helper.rb
@@ -2,7 +2,7 @@ module FriendsHelper
 
     def get_friend_link user, target
         return wrap_get_friend_link(link_to('Sign-up to Follow', signup_path)) if user.blank?
-        return '' unless user && target
+        return '' unless user != :false && target
         dom_id = user.dom_id(target.dom_id + '_friendship_')
         return wrap_get_friend_link('') if user == target
         return wrap_get_friend_link(link_to_remote( 'Stop Being Friends', :url => user_friend_path(user, target), :method => :delete), dom_id) if user.friend_of? target

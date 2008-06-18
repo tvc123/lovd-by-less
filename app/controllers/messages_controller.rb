11e07916842d4e7cac26efac90636ecdc41ee4d0
jbasdf <jbasdf@cosls-macbook-pro.local>
2008-06-18 17:04:55 -0600

    fixed user profile errors

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,17 +4,17 @@ class MessagesController < ApplicationController
   
   def index
     @message = Message.new
-    @to_list = @p.friends
+    @to_list = current_user.friends
     
-    if @p.received_messages.empty? && @p.has_network?
+    if current_user.received_messages.empty? && current_user.has_network?
       flash[:notice] = 'You have no mail in your inbox.  Try sending a message to someone.'
-      @to_list = (@p.followers + @p.friends + @p.followings)
-      redirect_to new_profile_message_path(@p) and return
+      @to_list = (current_user.followers + current_user.friends + current_user.followings)
+      redirect_to new_user_message_path(current_user) and return
     end
   end
   
   def create
-    @message = @p.sent_messages.create(params[:message]) 
+    @message = current_user.sent_messages.create(params[:message]) 
     
     respond_to do |format|
       if @message.new_record?
@@ -37,19 +37,19 @@ class MessagesController < ApplicationController
   
   def new
     @message = Message.new
-    @to_list = (@p.followers + @p.friends + @p.followings)
+    @to_list = (current_user.followers + current_user.friends + current_user.followings)
     render
   end
   
   
   def sent
     @message = Message.new
-    @to_list = @p.friends
+    @to_list = current_user.friends
   end
   
   def show
-    @message = @p.sent_messages.find params[:id] rescue nil
-    @message ||= @p.received_messages.find params[:id] rescue nil
+    @message = current_user.sent_messages.find params[:id] rescue nil
+    @message ||= current_user.received_messages.find params[:id] rescue nil
     @to_list = [@message.sender]
   end
   
@@ -63,6 +63,6 @@ class MessagesController < ApplicationController
   def can_send
     render :update do |page|
       page.alert "Sorry, you can't send messages. (Cuz you sux.)"
-    end unless @p.can_send_messages
+    end unless current_user.can_send_messages
   end
 end

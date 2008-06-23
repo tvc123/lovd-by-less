c95f6da58adc64f6731a1e7232c8c848d5d145fa
jbasdf <jbasdf@cosls-macbook-pro.local>
2008-06-23 16:13:56 -0600

    added code to create user folders in plone

--- app/models/user_plone_observer.rb
+++ app/models/user_plone_observer.rb
@@ -10,9 +10,17 @@ class UserPloneObserver < ActiveRecord::Observer
     end
 
     def after_create(user)
+        
+        require 'xmlrpc/client'
+        require 'pp'
+        
         group_member = PloneGroupRole.find_by_login(user.login) || PloneGroupRole.create(:login => user.login, :rolename => 'Member')
         open_contributor = PloneOpenRole.find_by_login(user.login) || PloneOpenRole.create(:login => user.login, :rolename => 'Contributor')
         open_member = PloneOpenRole.find_by_login(user.login) || PloneOpenRole.create(:login => user.login, :rolename => 'Member')
+        
+        server = XMLRPC::Client.new(GlobalConfig.plone_xmlrpc_server, GlobalConfig.plone_xmlrpc_path, GlobalConfig.plone_xmlrpc_port, nil, nil, user.login, user.password, false, 2000)   
+        result = server.call2('xmlrpc_createMemberArea', user.login)
+        
     end
     
 end

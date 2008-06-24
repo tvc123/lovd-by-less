be3f4fd189bda0f6f275e19b81adac7aa932f110
jbasdf <jbasdf@cosls-macbook-pro.local>
2008-06-24 17:27:07 -0600

    committing code with backgrounddrb integrated into branch

--- config/environment.rb
+++ config/environment.rb
@@ -28,3 +28,12 @@ Rails::Initializer.run do |config|
 
 end
 
+# load in global var here so that initializers can use them
+
+require 'ostruct'
+require 'yaml'
+
+config = OpenStruct.new(YAML.load_file("#{RAILS_ROOT}/config/global_config.yml"))
+env_config = config.send(RAILS_ENV)
+config.common.update(env_config) unless env_config.nil?
+::GlobalConfig = OpenStruct.new(config.common)
--- config/initializers/global_config.rb
+++ config/initializers/global_config.rb
@@ -1,7 +1,7 @@
-require 'ostruct'
-require 'yaml'
-
-config = OpenStruct.new(YAML.load_file("#{RAILS_ROOT}/config/global_config.yml"))
-env_config = config.send(RAILS_ENV)
-config.common.update(env_config) unless env_config.nil?
-::GlobalConfig = OpenStruct.new(config.common)
+# require 'ostruct'
+# require 'yaml'
+# 
+# config = OpenStruct.new(YAML.load_file("#{RAILS_ROOT}/config/global_config.yml"))
+# env_config = config.send(RAILS_ENV)
+# config.common.update(env_config) unless env_config.nil?
+# ::GlobalConfig = OpenStruct.new(config.common)

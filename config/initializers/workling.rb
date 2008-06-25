e0b496d998ac80722bd0ab4d9c9379fc1d9fd0c0
jbasdf <jbasdf@cosls-macbook-pro.local>
2008-06-25 09:56:12 -0600

    added spawn and instruction on how to use workling

--- config/initializers/workling.rb
+++ config/initializers/workling.rb
@@ -1,5 +1,12 @@
-# sudo starling -d
+# Get starling and working going with:
+#
+# Fire up starling - this isn't in daemon mode.  Add a -d for that.
+# starling -P tmp/pids/starling.pid -q tmp/starling 
+#
+# Next fire up the server
 # script/server
+#
+# Get workling going
 # script/workling_starling_client start
 
 # if you comment out the following line all calls will be syncronous - useful for debugging

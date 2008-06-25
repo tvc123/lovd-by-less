25801063e364a9d1ff98da8eae7b62ed93c89842
jbasdf <jbasdf@cosls-macbook-pro.local>
2008-06-25 13:29:25 -0600

    move plone code into model

--- app/workers/plone_worker.rb
+++ app/workers/plone_worker.rb
@@ -6,30 +6,11 @@ class PloneWorker < Workling::Base
 
         user = User.find(options[:user_id])
         if user
-            begin 
-                
-                logger.info "***********************************************************************"
-                logger.info "Adding plone groups" 
-                group_member = PloneGroupRole.find_by_login(user.login) || PloneGroupRole.create(:login => user.login, :rolename => 'Member')
-                open_contributor = PloneOpenRole.find_by_login(user.login) || PloneOpenRole.create(:login => user.login, :rolename => 'Contributor')
-                open_member = PloneOpenRole.find_by_login(user.login) || PloneOpenRole.create(:login => user.login, :rolename => 'Member')
-
-                logger.info "Creating user folder on plone site"
-                server = XMLRPC::Client.new(GlobalConfig.plone_xmlrpc_server, GlobalConfig.plone_xmlrpc_path, GlobalConfig.plone_xmlrpc_port, nil, nil, user.login, options[:password], false, 2000)   
-                result = server.call2('xmlrpc_createMemberArea', user.login)
-
-                if result[0] == false
-                    logger.info result[1].faultString
-                end
-                
-                logger.info "***********************************************************************"
-            rescue Exception => e
-                logger.info "Error setting up user's plone integration: " + e.message
-                # something failed try again later
+            if !Plone.user_to_plone.user_to_plone(user, options[:password])
                 PloneWorker.asynch_process_plone_users(:user_id => user.id)
-            end 
+            end
         end
-
+        
     end
     
 end

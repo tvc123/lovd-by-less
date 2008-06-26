5addde250636b4c5d09c76dd6809c7f3f1f343ee
jbasdf <jbasdf@cosls-macbook-pro.local>
2008-06-26 00:05:44 -0600

    testing

--- app/models/plone.rb
+++ app/models/plone.rb
@@ -11,17 +11,18 @@ class Plone
            open_contributor = PloneOpenRole.find_by_login(user.login) || PloneOpenRole.create(:login => user.login, :rolename => 'Contributor')
            open_member = PloneOpenRole.find_by_login(user.login) || PloneOpenRole.create(:login => user.login, :rolename => 'Member')
 
-           server = XMLRPC::Client.new(GlobalConfig.plone_xmlrpc_server, GlobalConfig.plone_xmlrpc_path, GlobalConfig.plone_xmlrpc_port, nil, nil, user.login, password, false, 2000)   
+           server = XMLRPC::Client.new(GlobalConfig.plone_xmlrpc_server, GlobalConfig.plone_xmlrpc_path, GlobalConfig.plone_xmlrpc_port, 
+                                        nil, nil, user.login, password, false, 2000)   
            result = server.call2('xmlrpc_createMemberArea', user.login)
 
            if result[0] == false
-               #logger.error "error sending data to plone"
-               #logger.error result[1].faultString
+               logger.error "error sending data to plone"
+               logger.error result[1].faultString
                success = false
            end
            
        rescue Exception => e
-           #logger.error "Error setting up user's plone integration: " + e.message
+           logger.error "Error setting up user's plone integration: " + e.message
            success = false
        end
        
--- config/initializers/workling.rb
+++ config/initializers/workling.rb
@@ -10,7 +10,7 @@
 # script/workling_starling_client start
 
 # if you comment out the following line all calls will be syncronous - useful for debugging
-Workling::Remote.dispatcher = Workling::Remote::Runners::StarlingRunner.new
+#Workling::Remote.dispatcher = Workling::Remote::Runners::StarlingRunner.new
 
 #this runner just executes everything normally
 #Workling::Remote.dispatcher = Workling::Remote::Runners::NotRemoteRunner.new
